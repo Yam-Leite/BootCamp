37441e48ba43a1ca54eb65981b88a99b49ea2340
Yam-Leite <100392402+Yam-Leite@users.noreply.github.com>
2024-09-22 11:33:10 -0300

    Update Mentoria.java

--- Mentoria.java
+++ Mentoria.java
@@ -1,33 +1,34 @@
 package DominioBootcamp;
 import java.time.LocalDate;
 
-public class Mentoria extends Conteudo {
-    
-    
-
-LocalDate data;
 
 
-public Mentoria(){}
+public class Mentoria extends Conteudo{
 
+    private LocalDate data;
 
-public LocalDate getData() {
-    return data;
-}
-public void setData(LocalDate data) {
-    this.data = data;
-}
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20d;
+    }
 
-public double calcularXp(){
-    return XP_PADRAO + 20d;
-}
+    public Mentoria() {
+    }
 
+    public LocalDate getData() {
+        return data;
+    }
 
-@Override
-public String toString() {
-    return "Mentoria [data=" + data + ", getTitulo()=" + getTitulo() + ", getDescricao()=" + getDescricao() + "]";
-}
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
 
-
-
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", data=" + data +
+                '}';
+    }
 }
