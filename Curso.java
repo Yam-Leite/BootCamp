9a60964158bc6176b67124f63120967b1cf007ba
Yam-Leite <100392402+Yam-Leite@users.noreply.github.com>
2024-09-22 11:32:05 -0300

    Update Curso.java

--- Curso.java
+++ Curso.java
@@ -1,32 +1,32 @@
 package DominioBootcamp;
 
-public class Curso  extends Conteudo{
-    
+public class Curso extends Conteudo{
+
+    private int cargaHoraria;
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
+    }
+
+    public Curso() {
+    }
 
 
-private int cargaHoraria;
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
 
-public Curso(){}
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
 
-
-
-public int getCargaHoraria() {
-    return cargaHoraria;
-}
-public void setCargaHoraria(int cargaHoraria) {
-    this.cargaHoraria = cargaHoraria;
-}
-@Override
-public String toString() {
-    return "Curso [titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", cargaHoraria=" + cargaHoraria + "]";
-}
-@Override
-public double calcularXp() {
-    return XP_PADRAO * cargaHoraria;
-   
-}
-
-
-
-
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
 }
